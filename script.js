9eeb91327739db984a169aaca83818270ba26d73
gymagyel <227876535+gymagyel@users.noreply.github.com>
2025-11-10 13:14:14 +0000

    Add pseudocode and working getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+/* 
+getComputerChoice()
+Generate a random number between 0 and 2    
+If 0 - "rock"
+    1 - "paper"
+    2 - "scissors"
+return the choice
+*/
+
+function getComputerChoice() {
+    const n= Math.floor(Math.random() * 3);
+    if (n == 0) return "rock" ;
+    if (n == 1) return "paper" ;
+    return "scissors" ;
+}
+console.log(getComputerChoice());
